a37657f116cefb6ac6ffb4c6133910fd5be2fcde
thanhluan0309 <letranthanhluan03092001@gmail.com>
2023-01-02 20:00:00 +0700

    update back front

--- server/router/post.js
+++ server/router/post.js
@@ -2,6 +2,6 @@ const post = require("../controller/Postcontroller");
 
 const router = require("express").Router();
 const verifyToken = require("../midleware/user");
-router.post("/", verifyToken, post.addPost);
-router.get("/",post.getAllPost)
+router.post("/", post.addPost);
+router.get("/", post.getAllPost);
 module.exports = router;
